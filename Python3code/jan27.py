812129742da41669aca9a1d86a624adcaa494970
Karan Polobotu <polobotu@gmail.com>
2022-01-25 11:36:23 -0500

    Forgot to commit these code files

---
+++ Python3code/jan27.py
@@ -0,0 +1,25 @@
+def json_e1():
+
+    return 0
+
+def rng_e1():
+
+    return 0
+
+def numpy_e1():
+
+    return 0
+
+def pandas_e1():
+
+    return 0
+
+def matplot_e1():
+
+    return 0
+
+def db_e1():
+
+    return 0
+
+if __name__ == "__main__":
