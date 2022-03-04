e571f3814475d9620d08c623e84d54f8538b0e70
Karan Polobotu <polobotu@gmail.com>
2022-03-04 14:10:17 -0500

    feb 28 code, works but exceeds time limit

---
+++ Python3code/02-2022/feb28.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: list) -> list:
+
+        arr = []
+        for i in range(1, len(nums) + 1):
+            arr.append(i)
+
+        print(arr)
+
+        diff = []
+        for i in range(len(arr)):
+            if arr[i] not in nums:
+                diff.append(arr[i])
+
+        return diff
+
+if __name__ == "__main__":
+    test_arr = [4,3,2,7,8,2,3,1]
+
+    test_obj = Solution()
+    print(test_obj.findDisappearedNumbers(test_arr))
