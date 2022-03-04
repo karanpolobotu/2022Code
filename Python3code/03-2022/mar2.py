a277847326d6112fb114ce92ea23431d64fe5076
Karan Polobotu <polobotu@gmail.com>
2022-03-04 14:42:33 -0500

    march 2nd code, works but not accepted by leetcode

---
+++ Python3code/03-2022/mar2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseString(self, s: list) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s) - 1, -1, -1):
+            s.append(s[i])
+
+        s = s[len(s)//2:]
+        print(s)
+
+if __name__ == "__main__":
+
+    s = ["H","a","n","n","a","h"]
+    test_obj = Solution()
+    print(test_obj.reverseString(s))
