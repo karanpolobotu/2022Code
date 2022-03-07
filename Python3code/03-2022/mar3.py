83df52a21f4ec8423ac5943b4a30c43aa6166b25
Karan Polobotu <polobotu@gmail.com>
2022-03-07 16:42:16 -0500

    march 3rd code, surprisingly simple. The key for me is to write out things on paper before attempting them in IDE

---
+++ Python3code/03-2022/mar3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0
+
+        count, a, b = 0, len(needle), len(haystack)
+
+        for i in range(0, b - a + 1):
+
+            if haystack[i:i+a] == needle:
+                return i
+
+
+        return -1
